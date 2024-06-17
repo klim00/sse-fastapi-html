445f0761c6e9908478cb358f5c8baeee13a7de46
Edward <limkaize720@gmail.com>
2024-06-17 11:08:59 +0800

    html, css changes
    - Change class name and id name
    - Mock layout css

--- html-js/test.js
+++ html-js/test.js
@@ -1,15 +1,15 @@
 const title = "Hello World!";
-document.getElementById("title").innerHTML = title;
+document.getElementById("header").innerHTML = title;
 
 const content = document.getElementById("content");
 let data;
 
-const eventSource = new EventSource("http://127.0.0.1:8000/sse");
+// const eventSource = new EventSource("http://127.0.0.1:8000/sse");
 
-eventSource.onopen = () => {
-  console.log("EventSource connected");
-};
+// eventSource.onopen = () => {
+//   console.log("EventSource connected");
+// };
 
-eventSource.addEventListener("test", (e) => {
-  console.log(e.data);
-});
+// eventSource.addEventListener("test", (e) => {
+//   console.log(e.data);
+// });
